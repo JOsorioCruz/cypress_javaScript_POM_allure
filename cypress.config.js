09032cb42b7302ae1610e5856d13acd7f7616276
JairoEstudiante <josorioc@unicartagena.edu.co>
2024-08-27 00:40:19 -0500

    update commit

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,6 @@
 const { defineConfig } = require("cypress");
 //const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-const  configureAllureAdapterPlugins = require ('@mmisty/cypress-allure-adapter/plugins');
+const  {configureAllureAdapterPlugins} = require ('@mmisty/cypress-allure-adapter/plugins');
 
 module.exports = defineConfig({
   retries: 1,
