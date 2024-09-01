d92b10191de6fcd04b869d5f5cfc5255fe32c5a1
jairo alonso osorio cruz <josorioc@unicartagena.edu.co>
2024-08-31 19:13:18 -0500

    update commit actualizacion del package.json

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,4 @@
 const { defineConfig } = require("cypress");
-//const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const  {configureAllureAdapterPlugins} = require ('@mmisty/cypress-allure-adapter/plugins');
 
 module.exports = defineConfig({
